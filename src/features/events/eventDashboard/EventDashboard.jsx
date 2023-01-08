8f23760923d35e0963057c22e51eca10a36fcb0d
Sunil <62702438+SunilSathyanarayana95@users.noreply.github.com>
2023-01-08 22:49:02 +0530

    End of section3

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, {userState} from "react";
 import { Grid } from "semantic-ui-react";
 import EventForm from "../eventForm/EventForm";
 import EventList from "./EventList";
 import { sampleData } from "../../../app/api/sampleData";
 
-export default function EventDashboard() {
+export default function EventDashboard({formOpen, setFormOpen}) {
+  const [events, setEvents] = userState(sampleData);
+  
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={sampleData} />
+        <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
-        <EventForm />
+        {formOpen && 
+        <EventForm setFormOpen={setFormOpen}/> }
       </Grid.Column>
     </Grid>
   )
