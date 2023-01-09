d5c6798b2f54fa02f4f900f50ce91f587c99e6d8
Sunil <62702438+SunilSathyanarayana95@users.noreply.github.com>
2023-01-09 17:32:18 +0530

    End of section5

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function render() {
   root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
   );
 }
 
